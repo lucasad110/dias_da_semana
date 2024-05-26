d09a82859ca557631bc29d875a286440158df6ca
lucascoder <lucas.faraday@outlook.com>
2024-05-26 18:59:01 -0300

    Initial commit

---
+++ src/DiasDaSemana.java
@@ -0,0 +1,11 @@
+public class DiasDaSemana{
+    public static void main(String[] args) {
+        String[] dias={"segunda-feira","terça-feira","quarta feira","quinta-feira,sexta-feira","sabado","domingo"};
+
+
+        System.out.println("os dias da semana são : ");
+        for (int i=0; i < dias.length;i++){
+            System.out.println(dias[i]);
+        }
+    }
+}
